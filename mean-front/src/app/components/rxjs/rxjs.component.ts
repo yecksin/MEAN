16b6371588eabae321395ed2e6a66f99fa8eb393
Yecksin Mauricio <yecksin@gmail.com>
2022-02-10 23:25:57 -0500

    rxjs added

--- mean-front/src/app/components/rxjs/rxjs.component.ts
+++ mean-front/src/app/components/rxjs/rxjs.component.ts
@@ -7,23 +7,39 @@ import { Observable } from 'rxjs';
   styleUrls: ['./rxjs.component.scss']
 })
 export class RxjsComponent implements OnInit {
-
+  index:any = 0;
+  error:any = '';
   constructor() { }
 
   ngOnInit(): void {
-
+    let random = Math.floor(Math.random() * (10 - 1)) + 1;
     let obs$ = new Observable(observer=>{
       let i = -1
       setInterval(() => {
         i++
         observer.next(i);
-      }, 1000);
+        if (i === 5) {
+          observer.complete();
+        }
+        if (i === random) {
+          observer.error(`Es ${random} y es un error, no cumple el complete`);
+        }
+      }, 500);
 
     })
 
-    obs$.subscribe(resp=>{
-      console.log(resp)
-    })
+    obs$.subscribe(
+      resp=>{
+        console.log(resp)
+        this.index = resp;
+      },
+      err=>{
+        console.log(err)
+        this.error = err;
+      },
+      ()=> console.info("terminado")
+
+    )
 
   }
 
