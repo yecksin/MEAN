7984ed7c8b2819b5f59aedc9c856d24d7b5599a7
Yecksin Mauricio <yecksin@gmail.com>
2022-02-12 21:39:15 -0500

    validar si email existe

--- mean-back/controllers/usuarios.controller.js
+++ mean-back/controllers/usuarios.controller.js
@@ -1,3 +1,4 @@
+const {response} = require('express');
 const Usuario = require('../models/usuario.model')
 
 const getUsuarios = async (req, res) => {
@@ -10,14 +11,36 @@ const getUsuarios = async (req, res) => {
   })
 }
 
-const crearUsuario = async (req, res) => {
-  const usuario = new Usuario(req.body)
-  await usuario.save();
-  res.json({
-    ok: true,
-    message: `Usuario ${nombre} creado`,
-    usuario
-  })
+//? el response es para tener el tipado de response
+const crearUsuario = async (req, res = response) => {
+  const {email} = req.body;
+  try {
+
+    const exissteEmail = await Usuario.findOne({email})
+    if (exissteEmail) {
+      return res.status(400).json({
+        ok: true,
+        message: `Correo ya existe`
+      })
+    }
+
+    const usuario = new Usuario(req.body)
+    await usuario.save();
+    res.json({
+      ok: true,
+      message: `Usuario ${nombre} creado`,
+      usuario
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok:false,
+      msg:'Error inesperado'
+    });
+  }
+
+
+
 }
 
 module.exports = {
