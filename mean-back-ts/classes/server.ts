ddb1e3b06be8eb5d1e1c32b3b7a1a68093165e61
Yecksin Mauricio <yecksin@gmail.com>
2022-02-13 22:14:45 -0500

    Cors added

--- mean-back-ts/classes/server.ts
+++ mean-back-ts/classes/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/enviroment';
-
+import cors from 'cors';
 export default class Server{
     public app:express.Application;
     public port:number;
@@ -11,6 +11,7 @@ export default class Server{
     }
 
     start(callback:Function){
+        this.app.use( cors() )
         this.app.use(express.json())
         this.app.listen(this.port, callback())
     }
