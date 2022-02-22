2fdacf7c19d0b0e32f04e73fd23f024dc247cbeb
Yecksin Mauricio <yecksin@gmail.com>
2022-02-21 23:54:45 -0500

    Medicos validaciones

--- mean-back-js/routes/medicos.route.js
+++ mean-back-js/routes/medicos.route.js
@@ -15,9 +15,13 @@ const router = Router();
 router.get('/', validarJWT ,getMedicos);
 
 router.post('/', 
-    [validarJWT,check('nombre',"El nombre del medico es requerido").not().isEmpty(),
-    validarCampos]
- ,crearMedico);
+    [
+    validarJWT,
+    check('nombre',"El nombre del medico es requerido").not().isEmpty(),
+    check('hospital',"El hospital id debe ser valido").isMongoId(),
+    validarCampos
+    ],
+    crearMedico);
 
  router.put('/:id',
  []
