7810a3208839c674865a178ec82898ba2a46b2e3
Yecksin Mauricio <yecksin@gmail.com>
2022-02-24 00:29:15 -0500

    busqueda total

--- mean-back-js/controllers/busquedas.controller.js
+++ mean-back-js/controllers/busquedas.controller.js
@@ -1,9 +1,27 @@
+const Usuario = require('../models/usuario.model');
+const Medico = require("../models/medico.model")
+const Hospital = require("../models/hospital.model")
+
 const getBusqueda = async (req, res = response) => {
-   
+   const palabra = req.params.id;
+   const regex = new RegExp(palabra,'i')
+//    const usuarios = await Usuario.find({nombre: regex});
+//    const medicos = await Medico.find({nombre: regex});
+//     const hospitales = await Hospital.find({nombre: regex});
+
+    const [usuarios, medicos, hospitales] = await Promise.all([
+        Usuario.find({ nombre: regex }),
+        Medico.find({ nombre: regex }),
+        Hospital.find({ nombre: regex }),
+    ])
+
     try {
         res.json({
             ok: true,
-            palabra:req.params.id
+            palabra,
+            usuarios,
+            medicos,
+            hospitales
         })
     } catch (error) {
         console.log(error)
