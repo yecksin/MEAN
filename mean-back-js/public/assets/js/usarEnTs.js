68dbfa920bc62e5c820658db48ba673e30349610
Yecksin Mauricio <yecksin@gmail.com>
2022-03-06 13:50:52 -0500

    dist added in public back folder

---
+++ mean-back-js/public/assets/js/usarEnTs.js
@@ -0,0 +1,6 @@
+const usarjsEnTs = (num1,num2)=>{
+    //? aqui se pone puro js
+    console.log("Este mensaje sale de un archivo js que llamo en Typescript despues de inicilizarlo en el index")
+    console.log(num1*num2)
+
+};
