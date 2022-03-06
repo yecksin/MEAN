e62033870ee96061d595bda6e9bbe8a57bc319d5
Yecksin Mauricio <yecksin@gmail.com>
2022-03-06 15:14:48 -0500

    servir spa

--- mean-back-js/index.js
+++ mean-back-js/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+const path = require('path');
 const express = require('express');
 const cors = require('cors')
 
@@ -32,6 +32,12 @@ app.use('/api/medicos', require('./routes/medicos.route'));
 app.use('/api/login', require('./routes/auth.route'));
 app.use('/api/todo', require('./routes/busquedas.route'));
 app.use('/api/upload', require('./routes/uploads.route'));
+
+//? si no es ninguna ruta
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'public/index.html'))
+});
 
 app.listen(process.env.PORT,()=>{
     console.log(`Puerto corriendo en el ${process.env.PORT}`)
